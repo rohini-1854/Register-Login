37c1bc91c579f52e2d8d9caca0a6e769a274da00
rohini-1854 <rohinirohymsu20@gmail.com>
2025-09-16 20:19:18 +0530

    Initial commit with backend and frontend, ignoring .env & node_modules

--- back/server.js
+++ back/server.js
@@ -2,15 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB error:", err));
+
 
 app.use('/api/users', userRoutes);
 
